d0d1d4b78ae7982d55063b6bcd31682aa74a2c9f
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-02 21:22:49 +0400

    Project Setup

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<main>
+			<Header />
+			<Hero />
+			<Footer />
+		</main>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	darkMode: ['class'],
+	content: [
+		'./pages/**/*.{js,jsx}',
+		'./components/**/*.{js,jsx}',
+		'./app/**/*.{js,jsx}',
+		'./src/**/*.{js,jsx}',
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '960px',
+			xl: '1200px',
+		},
+		colors: {
+			white: '#fff',
+			black: '#273029',
+			soft_green: {
+				DEFAULT: '#85c7dc',
+				secondary: '#f3f9fb',
+			},
+			grey: {
+				DEFAULT: '#888',
+				secondary: '#f8f8f8',
+			},
+			orange: '#f2994a',
+			outline: '#f1f1f1',
+			pink: '#f3d1d7',
+			body: '#e5e5e5',
+		},
+		extend: {
+			keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
+		},
+		backgroundImage: {
+			hero: 'url(/hero/bg.png)',
+			hero2: 'url(/hero/bg-2.png)',
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
+};
