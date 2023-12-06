9fe561d9d6e392d389a79192cee47ae72be9a999
Kirill Lyashenko <tltelf@yandex.ru>
2023-12-06 16:54:43 +0400

    add Recommendation component

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Featured from '@/components/Featured';
+import Recommendation from '@/components/Recommendation';
 
 export default function Home() {
 	return (
@@ -11,6 +12,7 @@ export default function Home() {
 			<Hero />
 			<Featured />
 			<About />
+			<Recommendation />
 			<Footer />
 			<div className='h-[4000px]'></div>
 		</main>
---
+++ components/Recommendation.jsx
@@ -0,0 +1,153 @@
+'use client';
+
+// swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// swiper modules
+import { Autoplay } from 'swiper/modules';
+
+// swiper styles
+import 'swiper/css';
+
+//components
+import { Button } from './ui/button';
+import Image from 'next/image';
+
+const recommendationData = [
+	{
+		name: 'Shanghai Hotel',
+		image: '/recommendation/hotel-1.png',
+		location: 'Shanghai, China',
+		desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium minima officia laudantium officiis esse, vero facere voluptatem repudiandae deleniti eos.',
+		price: 40,
+		testimonal: {
+			message:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quaerat alias molestias esse officiis tempora.',
+			personName: 'Robert Rene',
+		},
+	},
+	{
+		name: 'Peaks Lodge',
+		image: '/recommendation/hotel-2.png',
+		location: 'Aspen, USA',
+		desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium minima officia laudantium officiis esse, vero facere voluptatem repudiandae deleniti eos.',
+		price: 80,
+		testimonal: {
+			message:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quaerat alias molestias esse officiis tempora.',
+			personName: 'Emily Jhonson',
+		},
+	},
+	{
+		name: 'Tropical Oasis Resort',
+		image: '/recommendation/hotel-3.png',
+		location: 'Bali, Indonesia',
+		desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium minima officia laudantium officiis esse, vero facere voluptatem repudiandae deleniti eos.',
+		price: 120,
+		testimonal: {
+			message:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quaerat alias molestias esse officiis tempora.',
+			personName: 'Michael Anderson',
+		},
+	},
+];
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/variants';
+
+const Recommendation = () => {
+	return (
+		<motion.section
+			variants={fadeIn('up', 0.2)}
+			initial='hidden'
+			whileInView='show'
+			viewport={{ once: false, amount: 0 }}
+			className='pb-12 xl:pt-[157px] xl:pb-[112px] bg-soft_green-secondary relative'
+		>
+			{/** slider */}
+			<motion.div
+				variants={fadeIn('up', 0.4)}
+				initial='hidden'
+				whileInView='show'
+				viewport={{ once: false, amount: 0 }}
+			>
+				<Swiper
+					modules={[Autoplay]}
+					autoplay={{ delay: 4000, disableOnInteraction: true }}
+					loop
+					speed={2000}
+				>
+					{recommendationData.map((hotel, index) => {
+						return (
+							<SwiperSlide key={index}>
+								<div className='flex flex-col xl:flex-row xl:pl-[135px] gap-y-12 xl:gap-y-0'>
+									{/** text */}
+									<div className='max-w-[345px] xl:pt-8 order-2 xl:order-1 text-center mx-auto xl:text-left xl:mx-0'>
+										<h2 className='h2 mb-4'>
+											{hotel.name}
+										</h2>
+										<p className='text-soft_green text-sm mb-6'>
+											{hotel.location}
+										</p>
+										<p className='mb-[60px]'>
+											{hotel.desc}
+										</p>
+										<div className='flex items-center gap-x-[50px]'>
+											<Button
+												variant='accent'
+												className='px-[44px]'
+											>
+												Book Now
+											</Button>
+											<div className='text-black'>
+												<span className='font-bold text-2xl'>
+													{hotel.price}
+												</span>
+												<span className='text-sm'>
+													/Night
+												</span>
+											</div>
+										</div>
+									</div>
+									{/** image & testimonial */}
+									<div className='order-1 xl:order-2 flex-1 flex justify-center xl:justify-end relative h-[634px]'>
+										<div>
+											<Image
+												src={hotel.image}
+												width={905}
+												height={528}
+												quality={100}
+												alt=''
+												className='xl:rounded-tl-[20px] xl:rounded-bl-[20px]'
+											/>
+											<div className='bg-soft_green p-4 text-white text-center xl:text-left xl:max-w-[468px] xl:min-h-[212px] xl:absolute xl:bottom-0 xl:translate-x-1/2 xl:rounded-xl xl:px-10'>
+												<p className='mb-3 max-w-md mx-auto xl:max-w-none xl:mx-0 xl:mb-6'>
+													{hotel.testimonal.message}
+												</p>
+												<p className='text-xl font-bold'>
+													{
+														hotel.testimonal
+															.personName
+													}
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</SwiperSlide>
+						);
+					})}
+				</Swiper>
+			</motion.div>
+			{/** pattern */}
+			<Image
+				src={'/recommendation/pattern.svg'}
+				width={240}
+				height={240}
+				alt=''
+				className='hidden xl:flex absolute left-[135px] -bottom-[120px]'
+			/>
+		</motion.section>
+	);
+};
+
+export default Recommendation;
